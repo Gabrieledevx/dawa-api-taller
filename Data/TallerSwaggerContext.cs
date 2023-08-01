9904b062a2d6113e3e543d8ae8a68943d10f6664
Gabriel Medranda <57644772+Gabrieledevx@users.noreply.github.com>
2023-07-31 22:49:25 -0500

    Add files via upload

---
+++ Data/TallerSwaggerContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using TallerSwagger.Models;
+
+namespace TallerSwagger.Data;
+
+public partial class TallerSwaggerContext : DbContext
+{
+    public TallerSwaggerContext(DbContextOptions<TallerSwaggerContext> options)
+        : base(options)
+    {
+    }
+
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.CourseId).HasName("PRIMARY");
+
+            entity.ToTable("courses");
+
+            entity.Property(e => e.CourseId)
+                .ValueGeneratedNever()
+                .HasColumnName("CourseID");
+            entity.Property(e => e.CourseName).HasMaxLength(45);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PRIMARY");
+
+            entity.ToTable("student");
+
+            entity.Property(e => e.StudentId).ValueGeneratedNever();
+            entity.Property(e => e.Lastname)
+                .HasMaxLength(45)
+                .HasColumnName("lastname");
+            entity.Property(e => e.Name)
+                .HasMaxLength(45)
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TallerSwagger.Models;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Lastname { get; set; }
+}
